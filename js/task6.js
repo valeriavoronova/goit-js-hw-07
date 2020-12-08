6c3158d73cb35f6eae38c90afe76006f7793c9e9
Valeria Voronova <66879713+valeriavoronova@users.noreply.github.com>
2020-12-09 00:22:02 +0200

    1234

--- js/task6.js
+++ js/task6.js
@@ -1,10 +1,18 @@
-document.getElementById("validation-input").onblur = function() {
-  console.log(this.value.length);
-  if (this.getAttribute('data-length') > this.value.length) { 
-    this.classList.remove('valid');
-    this.classList.add('invalid');
-  } else {
-    this.classList.remove('invalid');
-    this.classList.add('valid');
+let inputVal = document.getElementById("validation-input");
+
+let totalLenght = inputVal.getAttribute("data-length");
+let intTotallenght = parseInt(totalLenght, 10);
+
+inputVal.oninput = function() {
+  if (inputVal.value.length === intTotallenght) {
+    inputVal.classList.remove("invalid");
+    inputVal.classList.add("valid");
+  }
+  if (inputVal.value.length === 0) {
+    inputVal.classList.remove("valid");
+    inputVal.classList.remove("invalid");
+  }
+  if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
+    inputVal.classList.add("invalid");
   }
 };
