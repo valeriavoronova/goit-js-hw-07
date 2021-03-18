a7f28e9881b1adae153b130ab24a981dd5d99fcf
Valeria Voronova <66879713+valeriavoronova@users.noreply.github.com>
2021-03-18 12:44:14 +0200

    11

--- js/task6.js
+++ js/task6.js
@@ -1,6 +1,6 @@
 document.getElementById("validation-input").onblur =function() {
   console.log(this.value.length);
-  if (this.getAttribute('data-length') > this.value.length) { 
+  if (this.getAttribute('data-length') != this.value.length) { 
     this.classList.remove('valid');
     this.classList.add('invalid');
   } else {
