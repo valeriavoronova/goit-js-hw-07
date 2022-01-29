4de92a202028211e64fdae9360ddfef3a8d0cf12
valeriavoronova <valeriavoronova1990@gmail.com>
2022-01-29 15:38:16 +0200

    213

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+// create html
+const items = galleryItems.map(( { preview, original, description } ) => {
+    const listEl = `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+    return listEl;
+}).join("");
 
-console.log(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", items);
+//console.log(galleryItems);
