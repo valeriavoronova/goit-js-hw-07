696cc5767db35aaaee2c3a7e0268c1d9cef9d4f9
valeriavoronova <valeriavoronova1990@gmail.com>
2022-01-29 20:42:48 +0200

    123

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-//console.log(galleryItems);
-
 const galleryRef = document.querySelector(".gallery");
 // create html
 const items = galleryItems.map(( { preview, original, description } ) => {
